df99deda2c58320719467f36e8eac5c168a8769a
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-09-03 19:56:28 +0530

    Add files via upload

---
+++ Linked List/linked_list.h
@@ -0,0 +1,13 @@
+#define EMPTYNODE 0
+
+struct nodeQ{
+    short data;
+    struct nodeQ* next;
+};
+
+typedef struct nodeQ nodeQ_t;
+
+typedef enum{
+    LIST_FALSE = 0,
+    LIST_TRUE = 1,
+} status_t;
